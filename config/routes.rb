bde7da03c6e809b15f225fd9ff05865b27b455a9
Nguyen Ton Hung <hungnt@hoangkhang.com.vn>
2016-04-28 08:52:18 +0700

    update location menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /en|vi/ do
+  scope "(:locale)", locale: /vi|en/ do
     root "home#index"
     get "home" => "home#sao", as: :sao
     get "contact" => "contact#index", as: :contact
